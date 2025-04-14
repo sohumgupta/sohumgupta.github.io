795d762b8f041dc49320ea3021f93fe84ff61363
Sohum Gupta <35411690+sohumgupta@users.noreply.github.com>
2025-04-14 18:31:21 -0400

    Add sitemap infrastructure

---
+++ sitemap-routes.js
@@ -0,0 +1,10 @@
+export default (
+    [
+        { path: "/photography" },
+        { path: "/photography/*" },
+        { path: "/writing" },
+        { path: "/about" },
+        { path: "/about/resume" },
+        { path: "/" },
+    ]
+);
